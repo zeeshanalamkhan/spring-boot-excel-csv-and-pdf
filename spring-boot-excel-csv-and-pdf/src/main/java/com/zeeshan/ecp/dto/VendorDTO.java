5a334d6fc9647885564dffefbc6c2bea6613cf36
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2020-08-09 12:32:53 +0530

    VendorDTO class added

---
+++ spring-boot-excel-csv-and-pdf/src/main/java/com/zeeshan/ecp/dto/VendorDTO.java
@@ -0,0 +1,16 @@
+package com.zeeshan.ecp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class VendorDTO/* implements Serializable */ {
+
+	// private static final long serialVersionUID = 1L;
+
+	private String vType;
+
+	private long count;
+
+}
